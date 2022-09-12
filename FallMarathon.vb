d9ced2d1147eb4bf4376d05b1214a061d4094f52
Ayana Lighty <ayanajlighty@student.midlandstech.edu>
2022-09-11 23:14:09 -0400

    add FallMarathon

---
+++ FallMarathon.vb
@@ -0,0 +1,3 @@
+﻿Public Class FallMarathon
+
+End Class
--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.CPT185Homework.Form1
+            Me.MainForm = Global.CPT185Homework.FallMarathon
         End Sub
     End Class
 End Namespace
